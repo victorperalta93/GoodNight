0b9c1d8b5b3d761d238792beddf4ff6a96ece621
Victor Peralta <victorperalta.correo@gmail.com>
2019-08-14 23:12:40 +0200

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// Declaración de la clase MainPage.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace GoodNight
+{
+	/// <summary>
+	/// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
